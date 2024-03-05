2370d2a74c02f4b047c44bc991fc0cf3c9ccdc60
ricardoAlmeidaEtic <ricardo_almeida@eticalgarve.com>
2024-03-05 22:06:28 +0000

    adição dos projetos portfolio e pyloop em docker system.

---
+++ portfolio/Dockerfile
@@ -0,0 +1,3 @@
+FROM httpd:2.4
+
+COPY ./portfolio_files /usr/local/apache2/htdocs/
---
+++ pyloop/pyloop_files/app.py
@@ -0,0 +1,8 @@
+import time
+
+while(True):
+    io = input('Enter your name:')
+    print("user input: ", io)
+    time.sleep(1)
+
+
